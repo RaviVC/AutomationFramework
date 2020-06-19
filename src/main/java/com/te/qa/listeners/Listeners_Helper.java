a079dc7b0c9e79fd7d077af885955f5a5783b9c4
Ravi Chandrika <Ravi.Chandrika@cai.io>
2020-06-19 15:11:44 +0530

    pom.xml chnages Listeners commented

--- src/main/java/com/te/qa/listeners/Listeners_Helper.java
+++ src/main/java/com/te/qa/listeners/Listeners_Helper.java
@@ -1,50 +1,57 @@
-package com.te.qa.listeners;
-
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
-
-public class Listeners_Helper implements ITestListener{
-	
-	public void onTestStart(ITestResult result) {
-		System.out.println(result.getName()+ "********** Test Execution Started **********");
-		System.out.println("\n");
-	}
-
-	public void onTestSuccess(ITestResult result) {
-		System.out.println(result.getName()+ " ->> Test Passed ");
-		System.out.println("********** Test Execution Completed **********");
-		System.out.println("*****************************************************************************************************");
-		System.out.println("\n");
-	}
-
-	public void onTestFailure(ITestResult result) {
-		System.out.println(result.getName()+ "**********Test Failed  **********");
-		System.out.println("*****************************************************************************************************");
-		
-	}
-
-	public void onTestSkipped(ITestResult result) {
-		System.out.println(result.getName()+ "**********Test Skipped **********");
-		System.out.println("*****************************************************************************************************");
-		
-	}
-
-	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void onStart(ITestContext context) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void onFinish(ITestContext context) {
-		System.out.println(context.getName()+ "********** Execution Finished **********");// TODO Auto-generated method stub
-		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-		
-	}
-
-
-}
+/*
+ * package com.te.qa.listeners;
+ * 
+ * import org.testng.ITestContext; import org.testng.ITestListener; import
+ * org.testng.ITestResult;
+ * 
+ * public class Listeners_Helper implements ITestListener{
+ * 
+ * public void onTestStart(ITestResult result) {
+ * System.out.println(result.getName()+
+ * "********** Test Execution Started **********"); System.out.println("\n"); }
+ * 
+ * public void onTestSuccess(ITestResult result) {
+ * System.out.println(result.getName()+ " ->> Test Passed ");
+ * System.out.println("********** Test Execution Completed **********");
+ * System.out.println(
+ * "*****************************************************************************************************"
+ * ); System.out.println("\n"); }
+ * 
+ * public void onTestFailure(ITestResult result) {
+ * System.out.println(result.getName()+ "**********Test Failed  **********");
+ * System.out.println(
+ * "*****************************************************************************************************"
+ * );
+ * 
+ * }
+ * 
+ * public void onTestSkipped(ITestResult result) {
+ * System.out.println(result.getName()+ "**********Test Skipped **********");
+ * System.out.println(
+ * "*****************************************************************************************************"
+ * );
+ * 
+ * }
+ * 
+ * public void onTestFailedButWithinSuccessPercentage(ITestResult result) { //
+ * TODO Auto-generated method stub
+ * 
+ * }
+ * 
+ * public void onStart(ITestContext context) { // TODO Auto-generated method
+ * stub
+ * 
+ * }
+ * 
+ * public void onFinish(ITestContext context) {
+ * System.out.println(context.getName()+
+ * "********** Execution Finished **********");// TODO Auto-generated method
+ * stub System.out.println(
+ * "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+ * );
+ * 
+ * }
+ * 
+ * 
+ * }
+ */
